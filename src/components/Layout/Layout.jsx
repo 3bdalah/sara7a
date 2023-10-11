89dfbe2ab4efde606e31307d5979b3ce2803f84f
3bdalah <3bdallah.dev@gmail.com>
2023-10-11 18:06:15 +0300

    feat: added 'Not Found' and 'Protected Routes' components - Addition of 'Not Found' component for handling 404 errors. - Implementation of 'Protected Routes' to secure certain routes. - Completion of the protected profile route.

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,5 +1,14 @@
+import { Outlet } from "react-router-dom";
+import Header from "./../Header/Header";
+import Footer from "./../Footer/Footer";
 const Layout = () => {
-  return <div>Template Layout</div>;
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
 };
 
 export default Layout;
